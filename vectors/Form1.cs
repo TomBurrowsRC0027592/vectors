52a0e8dd4e2083ba00c8e909767c155f7304218c
Tom Burrows <rc0027592@student.riversidecollege.ac.uk>
2018-12-11 10:46:35 +0000

    made new angle calculation for task 5 incomplete

--- vectors/Form1.cs
+++ vectors/Form1.cs
@@ -16,5 +16,37 @@ namespace vectors
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox2.Text);
+                double f = double.Parse(textBox1.Text);
+                double Fx = f * Cos(a);
+                double Fy = f * Sin(a);
+                label3.Text = "Fy = " + Fy + " N";
+                label4.Text = "Fx = " + Fx + " N";
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number ");
+            }
+        }
+        double Sin(double a)
+        {
+            double rad = a / 180.0 / Math.PI;
+            return Math.Sin(rad);
+        }
+
+        double Inversesin(double a)
+        {
+            return Math.Asin(a / 180.0 * Math.PI);
+        }
+
+        double Cos(double a)
+        {
+            return Math.Cos(a / 180.0 * Math.PI);
+        }
     }
-}
+}        
